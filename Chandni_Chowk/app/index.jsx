d7e97726a1e33d2af521f90bb266e8ee39d94526
Akshat Ranjan <akshatranjan000@gmail.com>
2025-07-27 20:20:57 +0530

    fixed tailwind issues

--- Chandni_Chowk/app/index.jsx
+++ Chandni_Chowk/app/index.jsx
@@ -1,13 +1,20 @@
 
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "./constants/images";
 
 export default function Index() {
   return (
-    <SafeAreaView className="justify-center items-center">
-      <ImageBackground source={images.welcomeCartBg} resizeMode="cover" className="h-full w-full"  />
-      <View className="h-20 w-20 bg-white"></View>
+    <SafeAreaView className="h-full w-full bg-white">
+      <ImageBackground 
+        source={images.welcomeCartBg} 
+        resizeMode="cover" 
+        className="justify-center items-center h-full w-full"
+      >
+        <View className="h-20 w-20 bg-white">
+          <Text className="text-black text-center">Test</Text>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
--- Chandni_Chowk/tailwind.config.js
+++ Chandni_Chowk/tailwind.config.js
@@ -1,9 +1,10 @@
 // tailwind.config.js
-/ @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app//*.{js,jsx,ts,tsx}", // ✅ matches your app directory
+    "./app/**/*.{js,jsx,ts,tsx}", // ✅ matches your app directory
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
