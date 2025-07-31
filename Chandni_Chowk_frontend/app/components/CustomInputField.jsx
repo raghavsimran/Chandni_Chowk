5f4b8b6ca7cb429b4e1272e613da8448fb3d75ed
Akshat Ranjan <akshatranjan000@gmail.com>
2025-08-01 02:15:59 +0530

    Fixed input field sizing issues

--- Chandni_Chowk_frontend/app/components/CustomInputField.jsx
+++ Chandni_Chowk_frontend/app/components/CustomInputField.jsx
@@ -5,10 +5,13 @@ import React from 'react'
 const CustomInputField = ({title}) => {
   return (
     <View className="w-72">
-        <Text className="text-black text-lg font-sukar-bold ml-2 ">{title}</Text>
-        <View className="h-11 border-1 w-full focus:border-2 bg-white border-form-border rounded-lg text-lg">
-         <TextInput className="h-10 text-sm" />
-        </View>
+      <Text className="text-black text-lg font-sukar-bold ml-2 ">{title}</Text>
+      <View className="w-full h-12">
+        <TextInput 
+          className="px-4 h-full border-1 w-full focus:border-2 bg-white border-form-border rounded-lg font-sukar-regular text-lg" 
+          style={{textAlignVertical: 'center'}} 
+        />
+      </View>
     </View>
   )
 }
